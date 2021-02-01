ba4b6032eefec20a6237b1b689fca41c7d80df14
MegaStack <59016455+MegaStack@users.noreply.github.com>
2021-02-01 17:53:00 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,10 +9,21 @@ public class Main {
 	public static void main(String[] args) {
 		File inA = new File("a.txt");
 		File inB = new File("b.txt");
-
+		
+		System.out.print("File a: ");
 		String[] arrayInA = MyFileLoader.getWords(inA);
+		for (String string : arrayInA) {
+			System.out.print(string + " ");
+		}
+		System.out.println();
+		
+		System.out.print("File b: ");
 		String[] arrayInB = MyFileLoader.getWords(inB);
-
+		for (String string : arrayInB) {
+			System.out.print(string + " ");
+		}
+		System.out.println();
+				
 		String resultText = compareMyWords(arrayInA, arrayInB);
 		System.out.println(resultText);
 		creatFile(resultText);
@@ -20,13 +31,11 @@ public class Main {
 
 	public static String compareMyWords(String[] a, String[] b) {
 		StringBuilder sb = new StringBuilder();
-
+		
 		for (String string : a) {
-			int flag = 0;
 			for (String string2 : b) {			
-				if (string.equalsIgnoreCase(string2) && flag == 0) {
+				if (string.equalsIgnoreCase(string2)) {
 					sb.append(string + " ");
-					flag = 1;				
 				}
 			}
 		}
@@ -42,5 +51,4 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-
 }
